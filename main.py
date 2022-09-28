70d9a6e9b590d6a91450a2a19185871ac19e6bfd
Garrett Hurst <pidgions21@gmail.com>
2022-09-28 10:10:00 -0400

    swapped gui for cli

--- main.py
+++ main.py
@@ -1,15 +1,73 @@
-from run_cards import run_cards
+import os
+from run_cards import run_cards_terminal
 from get_words import get_words
 
 
-if __name__ == '__main__':
-    username = input('[+] Enter VHL username/email >> ')
-    password = input('[+] Enter VHL password >> ')
-    link = input('[+] Enter link for VHL page >> ')
-    # link = 'https://m3a.vhlcentral.com/sections/1276373/activities/238470'  # vhl link to vocab page
-    # creds =  tuple([x.strip() for x in open('login.txt', 'r').readlines()]) # saved creds in a local file
-    
-    words = get_words((username, password), link)
+save_directory = 'card_sets'
+splitting_sequence = '~|~'
 
-    run_cards(words)
+
+def save_cards(cards, fname):
+    if save_directory not in os.listdir('./'): os.mkdir(save_directory)
+    fname = save_directory + '/' + fname
+    with open(fname, 'w') as f:
+        for word, translation in cards:
+            f.write(f'{word}{splitting_sequence}{translation}\n')
+
+if __name__ == '__main__':
+    choice_keys = {
+        'scrape new': 1,
+        'run cards': 2,
+        'remove cards': 3,
+        'exit': 4
+    }
+
+    message =  'Select from the following options: \n'
+    message += '[{}] Scrape a new set of words\n'.format(choice_keys['scrape new'])
+    message += '[{}] Run flashcards from an existing set\n'.format(choice_keys['run cards'])
+    message += '[{}] Remove an existing set\n'.format(choice_keys['remove cards'])
+    message += '[{}] Close the program\n\n'.format(choice_keys['exit'])
+    message += 'What would you like to do >> '
+
     
+
+    run_program = True
+    while run_program:
+        try:
+            choice = int(input(message).strip())
+        except:
+            print('Invalid input')
+            continue
+        
+        # TODO: get_words does not strip the newline characters from the words when they are scrapred
+        if choice == choice_keys['scrape new']:
+            username = input('[+] Enter VHL username/email >> ')
+            password = input('[+] Enter VHL password >> ')
+            link     = input('[+] Enter link for VHL page >> ')
+            
+            words = get_words((username, password), link)
+            fname = input('[+] Enter name to save cards under >> ')
+            save_cards(words, fname)
+
+        elif choice == choice_keys['run cards']:
+            options = os.listdir('./' + save_directory)
+            print('Files avalable: ')
+            for i, o in enumerate(options):
+                print(f'\t[{i+1}] {o}')
+            set_selected = int(input('\tSelect a file to run >> '))
+            path = save_directory + '/' + options[set_selected-1]
+            data = [line.split(splitting_sequence) for line in open(path, 'r')]
+            run_cards_terminal(data)
+                    
+        elif choice == choice_keys['remove cards']:
+            pass
+
+        elif choice == choice_keys['exit']:
+            run_program = False
+        
+        else:
+            print('\nInvalid Option Detected: {}'.format(choice))
+
+
+        
+        
--- run_cards.py
+++ run_cards.py
@@ -1,22 +1,77 @@
 from tkinter import *
-from random import randint
+from random import randint, shuffle
+from copy import copy
+
+
+def run_cards_terminal(cards):
+    shuffle(cards)
+    cards = [(c.replace('\n', ''), v.replace('\n', '')) for c, v in cards]
+    cards_master_set = copy(cards)
+    def pop_random(c):
+        return c.pop(randint(0, len(c) - 1))
+    def get_random(c):
+        return c[randint(0, len(c) - 1)]
+        
+    def get_int(msg):
+        is_valid = False
+        while not is_valid:
+            try:
+                int_gotten = input(msg).strip().lower()[0]
+                if int_gotten == 'q' or int_gotten == 'e':
+                    return -1
+                int_gotten = int(int_gotten)
+                is_valid = True
+            except:
+                pass
+        return int_gotten
+        
+    run = True
+    while(run and len(cards) > 0):
+        answer = pop_random(cards)
+        choices = [answer[1]]
+        for _ in range(3):
+            choices.append(get_random(cards_master_set)[1])
+        shuffle(choices)
+        print(f'Translate: {answer[0]}')
+        for i, choice in enumerate(choices):
+            print(f'[{i}] {choice}')
+        
+        # sentinal value of -1 to exit
+        choice = get_int('Enter choice (quit or exit to leave)>> ')
+        if choice == -1:
+            run = False
+        while( run and choices[choice] != answer[1]):
+            print('Incorrect')
+            choice = get_int('Enter choice (quit or exit to leave)>> ')
+            if choice == -1:
+                run = False
+
+            
+    
+
+    
+
 
 '''
 Citation:
 This code was largly adapted from: https://github.com/flatplanet/Intro-To-TKinter-Youtube-Course/blob/master/language.py
+
+Note:
+currently changed to a terminal based card running apprach because I liked it a  little better for now.
+Working on making the gui a little more coustomized for this spesific task. 
 '''
 
 def run_cards(words):
         
     root = Tk()
-    root.title('German Flashcard GUI')
+    root.title('Flashcards GUI')
     root.geometry("550x450")
 
     # get a count of our word list
     count = len(words)
 
     def next():
-        nonlocal hinter, hint_count
+        global hinter, hint_count
         # Clear the screen
         answer_label.config(text="")
         my_entry.delete(0, END)
